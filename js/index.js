a27082ff640a74bd5b561dca2ce6f68d6154b442
james stimpson <jws19900715@gmail.com>
2020-07-10 23:50:40 -0400

    set up addevent to first photo, need to rescale to proper sizing.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+const firstImg = document.querySelector("img[src = 'img/fun-bus.jpg']");
+firstImg.addEventListener("mouseenter", () => {
+  firstImg.style.transform = "scale(5.0)";
+  firstImg.style.transition = "all 0.3s";
+});
+
+firstImg.addEventListener("mouseleave", () => {
+  firstImg.style.transform = "scale(1.0)";
+  firstImg.style.transition = "all 0.3s";
+});
