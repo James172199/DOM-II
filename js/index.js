7f8d560de644531f1800ec078d4ec955b8d60bcf
james stimpson <jws19900715@gmail.com>
2020-07-13 21:18:29 -0400

    fixed scale, continuing on with project

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 // Your code goes here
 const firstImg = document.querySelector("img[src = 'img/fun-bus.jpg']");
 firstImg.addEventListener("mouseenter", () => {
-  firstImg.style.transform = "scale(5.0)";
+  firstImg.style.transform = "scale(2.5)";
   firstImg.style.transition = "all 0.3s";
 });
 
